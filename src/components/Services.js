a2f5b661672a0d6c45096f78ac1e1b315f2057ee
krittiyaclark <krittiyaclark@gmail.com>
2020-06-30 16:51:20 -0400

    Rendering a Services component

---
+++ src/components/Services.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from 'react-icons/fa';
+import Titile from '../components/Titile';
+
+class Services extends Component {
+	state = {
+		services: [
+			{
+				icons: <FaCocktail />,
+				title: 'free cocktails',
+				info:
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			},
+			{
+				icons: <FaHiking />,
+				title: 'Endless Hiking',
+				info:
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			},
+			{
+				icons: <FaShuttleVan />,
+				title: 'free shuttle',
+				info:
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			},
+			{
+				icons: <FaBeer />,
+				title: 'strongest beer',
+				info:
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			},
+		],
+	};
+	render() {
+		return (
+			<section className='services'>
+				<Titile title='services' />
+				<div className='services-center'>
+					{this.state.services.map((item, index) => {
+						return (
+							<article key={index} className='service'>
+								<span>{item.icons}</span>
+								<h6>{item.title}</h6>
+								<p>{item.info}</p>
+							</article>
+						);
+					})}
+				</div>
+			</section>
+		);
+	}
+}
+
+export default Services;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,20 @@ import { Link } from 'react-router-dom';
 
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
+import Services from '../components/Services';
 
 const Home = () => {
 	return (
-		<Hero>
-			<Banner title='luxurious room' subtitle='deluxe rooms starting at $299'>
-				<Link to='/rooms' className='btn-primary'>
-					our rooms
-				</Link>
-			</Banner>
-		</Hero>
+		<>
+			<Hero>
+				<Banner title='luxurious room' subtitle='deluxe rooms starting at $299'>
+					<Link to='/rooms' className='btn-primary'>
+						our rooms
+					</Link>
+				</Banner>
+			</Hero>
+			<Services />
+		</>
 	);
 };
 
