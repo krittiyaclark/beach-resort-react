644cabf9f72734bcb6bb09f4345c59a30300adb9
krittiyaclark <krittiyaclark@gmail.com>
2020-07-02 09:45:33 -0400

    Intergating the filter for the search rooms in the RoomsFilter component with Context

--- src/Context.js
+++ src/Context.js
@@ -10,6 +10,16 @@ class RoomProvider extends Component {
 		sortedRooms: [],
 		featuredRooms: [],
 		loading: true,
+		// RoomsFilter
+		type: 'all',
+		capacity: 1,
+		price: 0,
+		minPrice: 0,
+		maxPrice: 0,
+		minSize: 0,
+		maxSize: 0,
+		breakfast: false,
+		pets: false,
 	};
 
 	// getData
@@ -18,38 +28,102 @@ class RoomProvider extends Component {
 		let rooms = this.formatData(items);
 		console.log(rooms);
 		let featuredRooms = rooms.filter((room) => room.featured === true);
+		let maxPrice = Math.max(...rooms.map((item) => item.price));
+		let maxSize = Math.max(...rooms.map((item) => item.size));
 		this.setState({
 			rooms,
-			sortedRooms: rooms,
 			featuredRooms,
+			sortedRooms: rooms,
 			loading: false,
+			//
+			price: maxPrice,
+			maxPrice,
+			maxSize,
 		});
 	}
 
 	formatData(items) {
-		let temItems = items.map((item) => {
-			// find id from data
+		let tempItems = items.map((item) => {
 			let id = item.sys.id;
-			// find image from data
 			let images = item.fields.images.map((image) => image.fields.file.url);
-			// copy room fields
-			let room = { ...item.fields, images: images, id: id };
 
+			let room = { ...item.fields, images, id };
 			return room;
 		});
-		return temItems;
+		return tempItems;
 	}
-
 	getRoom = (slug) => {
-		// Get a specific room that has a slug
-		let tempRooms = [...this.state.rooms]; // copy rooms
+		let tempRooms = [...this.state.rooms];
 		const room = tempRooms.find((room) => room.slug === slug);
 		return room;
+	};
+	handleChange = (event) => {
+		const target = event.target;
+		const value = target.type === 'checkbox' ? target.checked : target.value;
+		const name = event.target.name;
+		console.log(name, value);
+
+		this.setState(
+			{
+				// Check the value of the input [name] that set the same as in the state
+				[name]: value,
+			},
+			this.filterRooms
+		);
+	};
+	filterRooms = () => {
+		let {
+			rooms,
+			type,
+			capacity,
+			price,
+			minSize,
+			maxSize,
+			breakfast,
+			pets,
+		} = this.state;
+		// all the rooms
+		let tempRooms = [...rooms];
+		// transform values
+		// get capacity
+		capacity = parseInt(capacity);
+		// get price
+		price = parseInt(price);
+		// filter by type
+		if (type !== 'all') {
+			tempRooms = tempRooms.filter((room) => room.type === type);
+		}
+		// filter by capacity
+		if (capacity !== 1) {
+			tempRooms = tempRooms.filter((room) => room.capacity >= capacity);
+		}
+		// filter by price
+		tempRooms = tempRooms.filter((room) => room.price <= price);
+		// filter by size
+		tempRooms = tempRooms.filter(
+			(room) => room.size >= minSize && room.size <= maxSize
+		);
+		//filter by breakfast
+		if (breakfast) {
+			tempRooms = tempRooms.filter((room) => room.breakfast === true);
+		}
+		// filter by pets
+		if (pets) {
+			tempRooms = tempRooms.filter((room) => room.pets === true);
+		}
+		this.setState({
+			sortedRooms: tempRooms,
+		});
 	};
 
 	render() {
 		return (
-			<RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+			<RoomContext.Provider
+				value={{
+					...this.state,
+					getRoom: this.getRoom,
+					handleChange: this.handleChange,
+				}}>
 				{this.props.children}
 			</RoomContext.Provider>
 		);
